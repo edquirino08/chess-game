74134b7b9d943b0c729fba49b8784f7aa36d50ca
Eduardo Quirino <101748384+edquirino08@users.noreply.github.com>
2022-11-13 21:03:17 -0300

    First class Position

---
+++ src/main/java/boardGame/Position.java
@@ -0,0 +1,55 @@
+package boardGame;
+
+public class Position {
+	
+	private int row; 
+	private int coloumn;
+	
+	
+	
+	public Position() {
+	}
+
+
+
+	public Position(int row, int coloumn) {
+		this.row = row;
+		this.coloumn = coloumn;
+	}
+
+
+
+	public int getRow() {
+		return row;
+	}
+
+
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+
+
+	public int getColoumn() {
+		return coloumn;
+	}
+
+
+
+	public void setColoumn(int coloumn) {
+		this.coloumn = coloumn;
+	}
+
+
+
+	@Override
+	public String toString() {
+		
+		return row + ", "+coloumn;
+	} 
+	
+	
+	
+
+}
