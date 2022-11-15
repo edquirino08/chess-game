34e786a46235557ac3cc20515d0834e7d50e5ecf
Eduardo Quirino <101748384+edquirino08@users.noreply.github.com>
2022-11-15 15:06:23 -0300

    Colocando as peças no tabuleiro

--- src/main/java/boardGame/Board.java
+++ src/main/java/boardGame/Board.java
@@ -37,4 +37,11 @@ public class Board {
 	public Piece piece (Position position) {
 		return pieces[position.getRow()][position.getColoumn()];
 	}
+	
+	public void placePiece(Piece piece , Position position) {
+		pieces[position.getRow()][position.getColoumn()] = piece;
+		piece.position = position;
+		
+		
+	}
 }
