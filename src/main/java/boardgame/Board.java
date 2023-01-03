3331b528a4bf0eace684cc2f1b94b04aa66f6125
Eduardo Quirino <101748384+edquirino08@users.noreply.github.com>
2023-01-03 15:47:38 -0300

    Final

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -7,6 +7,8 @@ public class Board {
 	private Piece[][] pieces;
 
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1)
+			throw new BoardException("Erro criando o tabuleiro! Deve haver pelo menos 1 linha e 1 coluna");
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -16,30 +18,29 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-
 	public Piece piece(int row, int column) {
+		if(!positionExists(row,column)){
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 
 	}
 
 	public Piece piece(Position position) {
+		if(!positionExists(position)){
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColoumn()];
 	}
 
 	public void placePiece(Piece piece, Position position) {
 		pieces[position.getRow()][position.getColoumn()] = piece;
 		piece.position = position;
+	
 
 	}
 
@@ -52,8 +53,8 @@ public class Board {
 		return positionExists(position.getRow(), position.getColoumn());
 
 	}
-	
-	public boolean thereIsAPiece (Position position) {
-		return piece(position) !=null;
+
+	public boolean thereIsAPiece(Position position) {
+		return piece(position) != null;
 	}
 }
